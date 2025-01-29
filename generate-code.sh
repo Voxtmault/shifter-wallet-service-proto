5f1dc9430608b03345e7a5e0298b90fb3fafec64
Andy <voxtmault@gmail.com>
2025-01-29 19:46:08 +0700

    added shifter service and get frontliner bulk rpc call

--- generate-code.sh
+++ generate-code.sh
@@ -8,14 +8,17 @@ export PATH="$PATH:$(go env GOPATH)/bin"
 # Generate Go Codes
 protoc --go_out=./go --go-grpc_out=./go wallet.proto
 protoc --go_out=./go --go-grpc_out=./go transaction.proto
+protoc --go_out=./go --go-grpc_out=./go shifter.proto
 echo "Go Codes Generated"
 
 # Generate JavaScript code
 protoc --js_out=import_style=commonjs,binary:./js/wallet-service wallet.proto
 protoc --js_out=import_style=commonjs,binary:./js/transaction-service transaction.proto
+protoc --js_out=import_style=commonjs,binary:./js/shifter-service shifter.proto
 echo "JS Code Generated"
 
 # Generate gRPC-Web code
 protoc --grpc-web_out=import_style=commonjs,mode=grpcwebtext:./web-js/wallet-service wallet.proto
 protoc --grpc-web_out=import_style=commonjs,mode=grpcwebtext:./web-js/transaction-service transaction.proto
+protoc --grpc-web_out=import_style=commonjs,mode=grpcwebtext:./web-js/shifter-service shifter.proto
 echo "JS Web Generated"
